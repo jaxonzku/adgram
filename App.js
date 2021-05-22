e2c3e0989c598d8386d9ecd733b71fd72e981bfc
jaxonzku <37749192+jaxonzku@users.noreply.github.com>
2021-05-22 23:38:56 +0530

    first page

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import Expo from'expo';
+
+
+import {
+  StyleSheet,
+  Button,
+  Text,
+  View,
+  Alert,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <View style={styles.title}></View>
+      <View style={styles.title}>
+        <Text style={styles.Adgram}>Adgram</Text>
+      </View>
+      <View style={styles.bottom}>
+        <View style={styles.bottom2}></View>
+        <View style={styles.influencer}>
+          <Button
+            title="INFLUENCER"
+            onPress={() => Alert.alert("Simple Button pressed")}
+          />
+        </View>
+        <View style={styles.influencer}>
+          <Button
+            title="       ADVERTISER       "
+            color="green"
+            onPress={() => Alert.alert("Simple Button pressed")}
+          />
+        </View>
+        <View style={styles.bottom2}></View>
+      </View>
     </View>
   );
 }
@@ -14,8 +44,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontSize: 80,
+
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+    fontFamily: "LobsterTwo-BoldItalic",
+  },
+  title: {
+    alignContent: "center",
+    textAlign: "center",
+    flex: 1,
+  },
+
+  Adgram: {
+    fontSize: 40,
+    fontFamily: "LobsterTwo-BoldItalic",
+    color: "skyblue",
+  },
+  bottom: {
+    flex: 4,
+  },
+  bottom2: {
+    flex: 1,
+  },
+  influencer: {
+    fontFamily: "LobsterTwo-BoldItalic",
+    alignContent: "center",
+    flex: 1,
+  },
+  advertiser: {
+    color: "green",
+    alignContent: "center",
+
+    flex: 1,
   },
 });
